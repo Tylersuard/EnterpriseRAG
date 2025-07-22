8129e088321c5038535e735a31368a1ed3b5956c
Tyler <41713505+Tylersuard@users.noreply.github.com>
2025-07-21 23:49:18 -0700

    Update function_app.py

--- Ch06/function_app.py
+++ Ch06/function_app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from uuid import uuid4
 load_dotenv()
 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 cosmos_client = CosmosClient(
 url = os.environ.get("COSMOSDB_ENDPOINT"),
 credential = os.environ.get("COSMOSDB_KEY")
@@ -15,11 +16,11 @@ print("Cosmos client created")
 feedback_db = cosmos_client.get_database_client(os.environ.get("COSMOSDB_DATABASE_NAME"))
 feedback_container = feedback_db.get_container_client(os.environ.get("COSMOSDB_CONTAINER_NAME"))
 print("Cosmos container connected")
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 print("ran the app thing")
 logging.info("Ran the app thing")
 
-@app.route(route="cosmosdb_func")
+@app.function_name(name="cosmosdb-func")
+@app.route(route="cosmosdb_func", methods=["POST", "GET"])
 def store_user_feedback(req: func.HttpRequest) -> func.HttpResponse:
     question = req.params.get("question")
     answer = req.params.get("answer")
@@ -44,12 +45,11 @@ def store_user_feedback(req: func.HttpRequest) -> func.HttpResponse:
             status_code=500
         )
     return func.HttpResponse(
-    "Successfully stored feedback.",
-    status_code=200,
-    headers={
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type"
-    }
-)
-
+        "Successfully stored feedback.",
+        status_code=200,
+        headers={
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type"
+        }
+    )
